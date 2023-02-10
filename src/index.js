4686f9f6fa449b11735a18daaa4ab2897963afc9
Who is bueno? <113481412+whoisbueno@users.noreply.github.com>
2023-02-10 22:21:57 +0000

    Update: creating the constants

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import c from "colors";
-console.log(c.blue("Ok"))
+import chalk from "chalk";
+import Vanely from "src/constants/Vanely.js";
+export const client = new Vanely()
+
+client.connect().catch(err => console.error(err))
+
+export default client;
